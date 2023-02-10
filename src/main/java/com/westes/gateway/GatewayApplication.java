8c60ba5c24f95bb8e66705da339bfde708847ec7
Will-Estes <will_estes@yahoo.com>
2023-02-10 11:30:16 -0700

    enable tracing via micrometer

--- src/main/java/com/westes/gateway/GatewayApplication.java
+++ src/main/java/com/westes/gateway/GatewayApplication.java
@@ -1,11 +1,19 @@
 package com.westes.gateway;
 
+import com.westes.gateway.trace.logging.ObservationContextSnapshotLifter;
+import io.micrometer.context.ContextSnapshot;
 import java.util.Date;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.ApplicationListener;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.event.ContextRefreshedEvent;
+import reactor.core.publisher.Hooks;
+import reactor.core.publisher.Operators;
 
 @SpringBootApplication
 public class GatewayApplication {
@@ -33,5 +41,14 @@ public class GatewayApplication {
             .filters(f -> f.rewritePath("/westes/cards-service/(?<segment>.*)", "/${segment}")
                 .addResponseHeader("X-Response-Time", new Date().toString()))
             .uri("lb://CARDS-SERVICE")).build();
+  }
+
+  @ConditionalOnClass({ContextSnapshot.class, Hooks.class})
+  @ConditionalOnWebApplication(type = ConditionalOnWebApplication.Type.REACTIVE)
+  @Bean
+  public ApplicationListener<ContextRefreshedEvent> reactiveObservableHook() {
+    return event -> Hooks.onEachOperator(
+        ObservationContextSnapshotLifter.class.getSimpleName(),
+        Operators.lift(ObservationContextSnapshotLifter.lifter()));
   }
 }
